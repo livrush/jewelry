4f75b8b6e6ec0b7e69596a3f8fd7c56f4cf3bd71
Liv <w.phrush@gmail.com>
2023-12-07 11:01:09 -0600

    start adding details header

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import colors from "./lib/colors.json";
 import inventory from "./lib/inventory.json";
 import Card from './components/Card';
+import HeaderDetails from './assets/dynamic/header-details';
 
 function App() {
   const colorNames = Object.keys(colors);
@@ -11,19 +12,22 @@ function App() {
 
   return (
     <div className={`App bg bg-image-${color}`}>
-      <header className={`App-header bg bg-color-${color} bg-white`}>
+      <header className={`main-header bg bg-color-${color} bg-image-white`}>
         <img src={logo} width="750px" className="" alt="logo" />
       </header>
-      <div className="inventory-list">
-        {
-          Object.keys(inventory).map((collection) => {
-            return inventory[collection].map((item) => {
-              return (
-                <Card item={item} color={color} />
-              )
-            });
-          })
-        }
+      <div>
+        <HeaderDetails color={color} />
+        <div className="inventory-list">
+          {
+            Object.keys(inventory).map((collection) => {
+              return inventory[collection].map((item) => {
+                return (
+                  <Card item={item} color={color} />
+                )
+              });
+            })
+          }
+        </div>
       </div>
       <footer className={`footer bg bg-color-${color} bg-image-white`}>All rights reserved.</footer>
     </div>
