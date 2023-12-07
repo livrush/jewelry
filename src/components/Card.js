c0b52e46bba3c5bcc8451973bc4c3cbbe2af4c14
Liv <w.phrush@gmail.com>
2023-12-07 09:31:28 -0600

    style cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,11 @@
+import curve from '../media/card-curve.svg';
+
 const Card = ({ item }) => {
   return (
     <div className="card">
-      {item.name} {item.category}
+      <img className="card-image" src={`https://picsum.photos/200?random=${item.name}`} alt="jewelry" />
+      <h2 className="card-name">{item.name}</h2>
+      <img className="card-curve" src={curve} alt="aesthetic curve along bottom and right side of the card" />
     </div>
   );
 }
